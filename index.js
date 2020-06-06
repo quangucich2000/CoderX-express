d2b6174a6b8f87987aaaeb184369e38f54ad7755
unknown <=>
2020-06-06 21:12:06 +0700

    B1-express

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function(request, response){
+	response.send('<h1>Hello !!</h1>')
+});
+
+app.listen(port, function(){
+	console.log('Server listening on port ' + port);
+});
